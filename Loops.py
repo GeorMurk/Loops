ba14c31a88e88f12524a79af225dc3ae11845204
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-10 03:00:02 +0300

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#!While Loops
+
+k = 1
+while k <= 10:
+    print(k)
+    k += 1
+
+print("Done")
+
+
+#!Go Ahead and Test it out!!!
---
+++ forloops.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#!Creating for_loops
+
+for letter in "python tutorial":
+    print(letter)
+
+#!with a variable
+people = ["George", "Chris", "Ivy", "Nick"]
+for name in people:
+    print(name)
+
+#!with indices
+for index in range(10):
+    print(index) #!note here its not going to print out 10 but rather is going to start from zero(0) to nine(9)
+
+for indices in range(2, 10):
+    print(indices) #!note here the numbers printed will start from 2 all the way to 9
+
+
+#!Go Ahead and Test it Out!!!!
